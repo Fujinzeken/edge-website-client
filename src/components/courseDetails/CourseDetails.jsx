1570198a5e7994da8046e653edaff8aae98ec836
micheal <fujinzeken@yahoo.com>
2023-07-06 18:08:07 +0100

    course Details

--- src/components/courseDetails/CourseDetails.jsx
+++ src/components/courseDetails/CourseDetails.jsx
@@ -1,14 +1,153 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Container, Row } from "reactstrap";
 import "./courseDetails.css";
 import Helmet from "../helmet/Helmet";
 import CommonSection from "../common-section/CommonSection";
+import ava1 from "../../images/ava-1.jpg";
+import ava2 from "../../images/ava-2.jpg";
+
 const CourseDetails = () => {
+  const [tab, setTab] = useState("overview");
+  console.log(tab);
   return (
     <Helmet title="Course Details">
       <CommonSection title="Course Name" />
       <Container>
-        <Row></Row>
+        <Row>
+          <Col lg="6" md="6" sm="6">
+            <div className=" d-flex gap-3 py-2 align-items-center">
+              <div
+                className={tab === "overview" ? "tabs__active" : "tabs"}
+                onClick={() => setTab("overview")}
+              >
+                <h6>Overview</h6>
+              </div>
+              <div
+                className={tab === "details" ? "tabs__active" : "tabs"}
+                onClick={() => setTab("details")}
+              >
+                <h6>Details</h6>
+              </div>
+              <div
+                className={tab === "instructor" ? "tabs__active" : "tabs"}
+                onClick={() => setTab("instructor")}
+              >
+                <h6>Instructor</h6>
+              </div>
+              <div
+                className={tab === "reviews" ? "tabs__active" : "tabs"}
+                onClick={() => setTab("reviews")}
+              >
+                <h6>Reviews</h6>
+              </div>
+            </div>
+            {tab === "overview" && (
+              <div className="rating__wrapper">
+                <div className="d-flex gap-4">
+                  <div
+                    className="d-flex flex-column align-items-start p-3"
+                    style={{
+                      background: "#fff",
+                      borderRadius: "10px",
+                      color: "#000",
+                    }}
+                  >
+                    <span>5 out of 5</span>
+                    <span>⭐⭐⭐⭐⭐</span>
+                    <span>3040 Students</span>
+                  </div>
+                  <div>
+                    <div className="d-flex flex-column gap-3">
+                      <span>Created by: Instructor Name</span>
+                      <span>
+                        Dolore dolore ea minim laboris.Culpa deserunt id aute
+                        anim sunt ea qui eu anim sit dolore sit duis magna.
+                        Lorem exercitation eiusmod in ullamco amet laboris
+                        occaecat voluptate.
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div style={{}}>
+                  <div>
+                    <div className="slider__content d-flex align-items-center gap-2">
+                      <img src={ava1} alt="customer-avatar" className="" />
+                      <h6>John Doe</h6>
+                    </div>
+                    <p className="review__text">
+                      Dolore dolore ea minim laboris.Culpa deserunt id aute anim
+                      sunt ea qui eu anim sit dolore sit duis magna. Lorem
+                      exercitation eiusmod in ullamco amet laboris occaecat
+                      voluptate.
+                    </p>
+                  </div>
+                  <div>
+                    <div className="slider__content d-flex align-items-center gap-2">
+                      <img src={ava2} alt="customer-avatar" className="" />
+                      <h6>John Doe</h6>
+                    </div>
+                    <p className="review__text">
+                      Dolore dolore ea minim laboris.Culpa deserunt id aute anim
+                      sunt ea qui eu anim sit dolore sit duis magna. Lorem
+                      exercitation eiusmod in ullamco amet laboris occaecat
+                      voluptate.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )}
+            {tab === "details" && (
+              <div className="rating__wrapper">
+                <div>
+                  <h2>What you will Learn</h2>
+                  <p>
+                    {" "}
+                    Dolore dolore ea minim laboris.Culpa deserunt id aute anim
+                    sunt ea qui eu anim sit dolore sit duis magna. Lorem
+                    exercitation eiusmod in ullamco amet laboris occaecat
+                    voluptate.
+                  </p>
+                </div>
+                <div>
+                  <h2>Requirements</h2>
+                  <ul className="list__ul">
+                    <Row>
+                      <Col lg="6">
+                        <li className="list">Own a Guitar</li>
+                      </Col>
+                      <Col lg="6">
+                        <li className="list">
+                          Basic knowledge of Music theory
+                        </li>
+                      </Col>
+                      <Col lg="6">
+                        <li className="list">
+                          Basic knowledge of Music theory
+                        </li>
+                      </Col>
+                      <Col lg="6">
+                        <li className="list">
+                          Basic knowledge of Music theory
+                        </li>
+                      </Col>
+                    </Row>
+                  </ul>
+                </div>
+                <div>
+                  <h2>Description</h2>
+                  <p>
+                    {" "}
+                    Dolore dolore ea minim laboris.Culpa deserunt id aute anim
+                    sunt ea qui eu anim sit dolore sit duis magna. Lorem
+                    exercitation eiusmod in ullamco amet laboris occaecat
+                    voluptate.
+                  </p>
+                </div>
+              </div>
+            )}
+          </Col>
+          <Col lg="6" md="6" sm="6"></Col>
+        </Row>
       </Container>
     </Helmet>
   );
--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -4,29 +4,30 @@ import { Link } from "react-router-dom";
 const Product = () => {
   const id = 1;
   return (
-    <div className="product__item shadow p-3 mb-5 rounded">
-      <div className="product__img mb-4">
-        <img src={guitar} alt="product__img" className="" />
-      </div>
-      <div className="product__name mb-3">
-        <h5>Name of product</h5>
-        <span>
-          Learn how to apply advanced guitar techniques to play even the most
-          complicated orchestra pieces
-        </span>
-      </div>
-      <div className="product__desc">
-        <span>4.5</span>
-        <span>275</span>
-        <span>Advanced</span>
-      </div>
-      <div className="product__btn">
-        <Link to={`/courses/${id}`}>
+    <Link to={`/courses/${id}`}>
+      <div className="product__item shadow p-3 mb-5 rounded">
+        <div className="product__img mb-4">
+          <img src={guitar} alt="product__img" className="" />
+        </div>
+        <div className="product__name mb-3">
+          <h5>Name of product</h5>
+          <span>
+            Learn how to apply advanced guitar techniques to play even the most
+            complicated orchestra pieces
+          </span>
+        </div>
+        <div className="product__desc">
+          <span>4.5</span>
+          <span>275</span>
+          <span>Advanced</span>
+        </div>
+        <div className="product__btn">
           <button>Learn More</button>
-        </Link>
-        <span>$20</span>
+
+          <span>$20</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
